716bc2ebb12b6471ab339a6839e42f5a6feaa6e5
aaa <aaa>
2024-06-11 21:46:39 +0800

    aaaaa

---
+++ static/info.js
@@ -0,0 +1,23 @@
+const companyMap = new Map();
+companyMap.set("1","东莞新能安科技有限公司")
+companyMap.set("2","星恒电源(滁州)有限公司-24V")
+companyMap.set("3","星恒电源(滁州)有限公司-48V")
+companyMap.set("4","山东爱德邦智能科技有限公司")
+companyMap.set("5","厦门新能安科技有限公司")
+companyMap.set("6","台州爱德邦智能科技有限公司")
+companyMap.set("7","浙江超威创元实业有限公司-24V")
+companyMap.set("8","浙江超恒动力科技有限公司")
+companyMap.set("9","湖州天丰电源有限公司")
+companyMap.set("10","浙江超威创元实业有限公司-48V")
+companyMap.set("11","山东爱德邦智能科技有限公司-48V")
+companyMap.set("12","旭派电源有限公司")
+companyMap.set("13","天能帅福得能源股份有限公司")
+
+const kgMap = new Map();
+kgMap.set("1","6.5±0.5(kg)")
+kgMap.set("2","7.5±0.5(kg)")
+kgMap.set("3","8.5±0.5(kg)")
+
+const typeMap = new Map();
+typeMap.set("1","锂电")
+typeMap.set("2","铅酸")
